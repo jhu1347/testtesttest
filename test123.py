e420fa576be208618422a0382a10a4824a5d9518
Jacob <40825709+jhu1347@users.noreply.github.com>
2022-11-22 22:07:04 +0800

    Create test123.py

---
+++ test123.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
+
+st.text("Hello World!")
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
+
+df = pd.DataFrame(np.random.randn(50, 20), columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)
